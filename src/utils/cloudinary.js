f42d591610f67d046a8a149251feb715f61439b5
PavanPawar12 <pavanpawar9923100@gmail.com>
2025-08-16 15:28:32 +0530

    commit

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import {vs as cloudinary} from 'cloudinary'
+import fs from 'fs' // fs: file system 
+
+cloudinary.config({ 
+        cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret:process.env.CLOUDINARY_API_SECRET 
+    });
+
+const uploadCloudinary = async (localFilePath) =>{
+    try {
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // file had been uploaded successfull
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)// remove the localy saved temporary file as the upload operation got gailed 
+    }
+}
+
+export {uploadCloudinary}
