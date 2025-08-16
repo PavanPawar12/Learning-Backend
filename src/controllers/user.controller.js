79235b53e0b19a7b1975d4b8e08e3c65ee4f624f
PavanPawar12 <pavanpawar9923100@gmail.com>
2025-08-17 01:13:21 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,61 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from '../utils/ApiError.js'
+import { User } from '../models/user.model.js'
+import {uploadOnCloudinary} from '../utils/cloudinary.js'
 
 const registerUser = asyncHandler(async (req, res)=>{
-    return res.status(200).json({
-        message:"Backend with pavan "
+   // get user details from backend
+   // validation - not empty
+   // check if user already exits: username, email 
+   // check for images, check for avtar
+   // upload them to cloudinary, avatar
+   // create user object - create antry in db
+   // remove password and refresh token field from response
+   // check for user creation 
+   // return res
+
+
+    const {fullName, email, username, password} = req.body
+    console.log("email: ",email);
+
+    // if(fullName == "") {
+    //     throw new ApiError(400, "fullname is required");
+    // }
+
+    if(
+        [fullName, email, username, password].some((field)=>
+        field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or:[{ username }, { email }]
     })
+    if(existedUser){
+        throw new ApiError(409, "User with email or username are already exists ")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocation = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is reqired")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocation)
+
+    if(!avatar) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage: coverImage?.url || "",
+    
+     })
 })
 
 
