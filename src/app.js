b852395ecc5a8817248f48b2b090af33893a0114
PavanPawar12 <pavanpawar9923100@gmail.com>
2025-08-19 10:53:17 +0530

    chages

--- src/app.js
+++ src/app.js
@@ -12,11 +12,11 @@ app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit:"16kb"})) //accept JSON data
+app.use(express.urlencoded({extended:true, limit:"16kb"})) // Read form body
+app.use(express.static("public")) // Serve static files
+app.use(cookieParser()) // Read cookies
+app.use(cookieParser()) 
 
 // routes import
 import userRouter from './routes/user.routes.js'
